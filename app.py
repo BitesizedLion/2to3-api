fd2b87991792ab891f768ff90add958626aecb28
Casper S <42980888+BitesizedLion@users.noreply.github.com>
2024-01-21 00:55:30 +0100

    Add health check endpoint

--- app.py
+++ app.py
@@ -18,5 +18,10 @@ def convert_script():
     return str(tree), 200
 
 
+@app.route("/health", methods=["GET"])
+def health_check():
+    return "OK", 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
